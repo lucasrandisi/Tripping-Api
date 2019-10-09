6ff26482bd53877902cd8e0b661bcbc01c8a31a7
lucasrandisi <lucasrandisi@gmail.com>
2019-10-09 15:52:32 -0300

    Session con JDBC

--- src/main/java/Triping/controllers/LogInController.java
+++ src/main/java/Triping/controllers/LogInController.java
@@ -3,12 +3,14 @@ package Triping.controllers;
 import Triping.services.IUserService;
 import Triping.utils.exceptions.HashingException;
 
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 
 @Controller
 @RequestMapping(path="/login")
@@ -17,9 +19,17 @@ public class LogInController {
     private IUserService userService;
 
     @PostMapping
-    public @ResponseBody String login(@RequestParam(value="usr") String username, @RequestParam(value="pwd") String password) {
+    public @ResponseBody String login(@RequestBody String data, HttpServletRequest request) {
+        Gson gson = new Gson();
+        UserDto userDto = gson.fromJson(data, UserDto.class);
+
+        String username = userDto.getUsername();
+        String password = userDto.getPassword();
+
         try {
             if( userService.validateUserCredentials(username, password) ){
+                HttpSession session = request.getSession();
+                session.setAttribute("logged", true);
                 return "Usuario encontrado";
             }
             else{
